bf39c1b2f54790557099f1ad75029d2e5dc8b85a
TheModel <152254955+TheModel@users.noreply.github.com>
2023-12-20 00:19:33 -0800

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -36,9 +36,7 @@ class WelcomeScreen(QMainWindow):
         self.widget.addWidget(createaccountscreen)
         self.widget.setCurrentIndex(self.widget.currentIndex() + 1)
 
-# Let me just do some version control stuff
-# wow it works
-#wow wowo
+
 
 
 
